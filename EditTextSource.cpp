44d87cde3010a875614363856f3bd443e838a160
Nekronomykon <nekronomykon@gmail.com>
2018-08-05 03:19:12 +0300

    text decorations

---
+++ EditTextSource.cpp
@@ -0,0 +1,6 @@
+#include "EditTextSource.h"
+
+EditTextSource::EditTextSource()
+{
+
+}
---
+++ EditTextSource.h
@@ -0,0 +1,17 @@
+#ifndef EDITTEXTSOURCE_H
+#define EDITTEXTSOURCE_H
+
+#include <QtCore/qglobal.h>
+#if QT_VERSION >= 0x050000
+#include <QtWidgets/QWidget>
+#else
+#include <QtGui/QWidget>
+#endif
+
+class EditTextSource
+{
+public:
+  EditTextSource();
+};
+
+#endif // EDITTEXTSOURCE_H
