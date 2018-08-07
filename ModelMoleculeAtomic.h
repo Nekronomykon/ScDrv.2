136895ec5abb8d1c62c114abe1d8ee3dd114231e
Nekronomykon <nekronomykon@gmail.com>
2018-08-08 02:01:12 +0300

    Further reform

--- ModelMoleculeAtomic.h
+++ ModelMoleculeAtomic.h
@@ -17,16 +17,21 @@ public:
   ModelMoleculeAtomic();
 
   int columnCount(const QModelIndex &/*parent*/ = QModelIndex()) const override;
-
-  int    rowCount(const QModelIndex &/*parent*/ = QModelIndex()) const override;
-
-  QVariant data(const QModelIndex & /*index*/, int /*role*/) const override;
-
-  QVariant headerData(int /*section*/, Qt::Orientation /*orientation*/
-    , int /*role*/ = Qt::DisplayRole) const override;
   
-  bool setData(const QModelIndex &/*index*/, const QVariant &/*value*/
-    , int /*role*/ = Qt::EditRole) override;
+  int rowCount(const QModelIndex &/*parent*/ = QModelIndex()) const override;
+  
+  QVariant data(const QModelIndex &
+    , int) const override;
+  
+  QVariant headerData(int section
+    , Qt::Orientation orientation
+    , int role = Qt::DisplayRole) const override;
+    
+    bool setData(const QModelIndex &index
+    , const QVariant &value
+    , int role = Qt::EditRole) override;
+
+     Qt::ItemFlags flags(const QModelIndex &index) const override;
 
 private:
   enum {
