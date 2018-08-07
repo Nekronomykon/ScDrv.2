30d2c2078b4df323eeaa9d48313a7b7e89e5dfa6
Nekronomykon <nekronomykon@gmail.com>
2018-08-07 04:29:21 +0300

    Some else

--- ModelMoleculeAtomic.cpp
+++ ModelMoleculeAtomic.cpp
@@ -1,7 +1,8 @@
 #include "ModelMoleculeAtomic.h"
 
 ModelMoleculeAtomic::ModelMoleculeAtomic()
-{}
+{
+}
 
 int ModelMoleculeAtomic::columnCount(const QModelIndex & /*parent*/) const
 {
@@ -15,34 +16,61 @@ int ModelMoleculeAtomic::rowCount(const QModelIndex & /*parent*/) const
 
 QVariant ModelMoleculeAtomic::data(const QModelIndex &mi, int role) const
 {
-  return (role == Qt::DisplayRole) ?
-    QVariant((float)1.25 + (mi.row() + mi.column()) / 3.5).toDouble() : QVariant();
+  return (role == Qt::DisplayRole) ? QVariant((float)1.25 + (mi.row() + mi.column()) / 3.5).toDouble() : QVariant();
 }
 
-QVariant ModelMoleculeAtomic::headerData(int section
-                                        , Qt::Orientation orientation
-                                        , int role
-                                        ) const
+QVariant ModelMoleculeAtomic::headerData(int section, Qt::Orientation orientation, int role) const
 {
-  if(orientation == Qt::Vertical || role != Qt::DisplayRole)
-    return QAbstractTableModel::headerData(section, orientation, role );
+  if (orientation == Qt::Vertical || role != Qt::DisplayRole)
+    return QAbstractTableModel::headerData(section, orientation, role);
   QVariant res;
-  switch (section) {
-    case (ColumnID): {res.setValue(tr("ID")); break;}
-    case (ColumnElement): {res.setValue(tr("Element")); break; }
-    case (ColumnType): {res.setValue(tr("Atom type")); break; }
-    case (ColumnLabel): {res.setValue(tr("User label")); break; }
-    case (ColumnX): {res.setValue(tr("X")); break; }
-    case (ColumnY): {res.setValue(tr("Y")); break; }
-    case (ColumnZ): {res.setValue(tr("Z")); break; }
-    default:  {res.setValue(tr("Comment")); break;}
-    }
+  switch (section)
+  {
+  case (ColumnID):
+  {
+    res.setValue(tr("ID"));
+    break;
+  }
+  case (ColumnElement):
+  {
+    res.setValue(tr("Element"));
+    break;
+  }
+  case (ColumnType):
+  {
+    res.setValue(tr("Atom type"));
+    break;
+  }
+  case (ColumnLabel):
+  {
+    res.setValue(tr("User label"));
+    break;
+  }
+  case (ColumnX):
+  {
+    res.setValue(tr("X"));
+    break;
+  }
+  case (ColumnY):
+  {
+    res.setValue(tr("Y"));
+    break;
+  }
+  case (ColumnZ):
+  {
+    res.setValue(tr("Z"));
+    break;
+  }
+  default:
+  {
+    res.setValue(tr("Comment"));
+    break;
+  }
+  }
   return res;
 }
 
-bool ModelMoleculeAtomic::setData(const QModelIndex &index
-, const QVariant &value
-, int role = Qt::EditRole)
+bool ModelMoleculeAtomic::setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole)
 {
- // stub... WUT? 
+  // stub... WUT?
 }
