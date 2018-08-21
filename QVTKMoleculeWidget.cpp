d8e5d5910544620bebf6d9d1ed485c36932cc521
Nekronomykon <nekronomykon@gmail.com>
2018-08-22 02:33:01 +0300

    Minor

--- QVTKMoleculeWidget.cpp
+++ QVTKMoleculeWidget.cpp
@@ -27,25 +27,23 @@ typedef vtkSmartPointer<vtkOpenGLRenderer> OpenGLRenderer;
 typedef vtkSmartPointer<vtkOpenGLMoleculeMapper> OpenGLMolMapper;
 
 const QVTKMoleculeMapStyle QVTKMoleculeWidget::style_Sticks =
-{ true, vtkMoleculeMapper::UnitRadius, 0.15f, true, false, vtkMoleculeMapper::DiscreteByAtom, 0.15f };
+    {true, vtkMoleculeMapper::UnitRadius, 0.15f, true, false, vtkMoleculeMapper::DiscreteByAtom, 0.15f};
 // [0] -> sticks
 
 const QVTKMoleculeMapStyle QVTKMoleculeWidget::style_BnS =
-{ true, vtkMoleculeMapper::VDWRadius, 0.25f, true, false, vtkMoleculeMapper::DiscreteByAtom, 0.125f };
+    {true, vtkMoleculeMapper::VDWRadius, 0.25f, true, false, vtkMoleculeMapper::DiscreteByAtom, 0.125f};
 // [1] -> balls and sticks
 
 const QVTKMoleculeMapStyle QVTKMoleculeWidget::style_VdW =
-{ true, vtkMoleculeMapper::VDWRadius, 1.0f, false, false, 0, 0 };
+    {true, vtkMoleculeMapper::VDWRadius, 1.0f, false, false, 0, 0};
 // [2] -> van der Waals
 
 const QVTKMoleculeMapStyle QVTKMoleculeWidget::style_Fast =
-{ true, vtkMoleculeMapper::UnitRadius, 0.375f, true, false, vtkMoleculeMapper::SingleColor, 0.1f };
+    {true, vtkMoleculeMapper::UnitRadius, 0.375f, true, false, vtkMoleculeMapper::SingleColor, 0.1f};
 // [3] -> fast internal
 
-
 QVTKMoleculeWidget::QVTKMoleculeWidget(QWidget *parent)
-    : QVTKOpenGLWidget(parent), mol_mapper_(OpenGLMolMapper::New())
-  , mol_style_(style_Fast)
+    : QVTKOpenGLWidget(parent), mol_mapper_(OpenGLMolMapper::New()), mol_style_(style_Fast)
 {
   // this->setAttribute(Qt::WA_NativeWindow, false);
   vtkNew<vtkGenericOpenGLRenderWindow> renderWindow;
@@ -74,12 +72,12 @@ QVTKMoleculeWidget::~QVTKMoleculeWidget() {}
 void QVTKMoleculeWidget::ShowMolecule(vtkMolecule *pMol)
 {
   mol_mapper_->RemoveAllInputs();
-  mol_style_.SetupMoleculeMapper(mol_mapper_);
   if (pMol)
   {
     mol_mapper_->SetInputData(pMol);
-    // mol_mapper_->Update();
   }
 
+  // render scene:
+  mol_style_.SetupMoleculeMapper(mol_mapper_);
   this->GetRenderWindow()->Render();
 }
