c90609e28c68bf262968f748a30c799183e7f3a7
Nekronomykon <nekronomykon@gmail.com>
2019-03-05 16:31:30 +0300

    Slighlty move on

--- CriticalPointStructure.cpp
+++ CriticalPointStructure.cpp
@@ -3,12 +3,18 @@ using namespace vtk;
 
 #include <vtkPoints.h>
 
+//-----------------------------------------------------------------------------
+vtkObjectFactoryNewMacro(CriticalPointStructure)
+// Can not yet understand what is wrong here
+
+//-----------------------------------------------------------------------------
 void CriticalPointStructure::Initialize()
 {
   points_->Initialize();
   this->Superclass::Initialize();
 }
 
+//-----------------------------------------------------------------------------
 void CriticalPointStructure::PrintSelf(ostream &os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os,indent);
