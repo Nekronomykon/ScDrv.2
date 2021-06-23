ef22053deb423c2cf0ae10f2eed4130e6822d1ec
Nekronomykon <nekronomykon@gmail.com>
2021-06-23 10:44:49 +0300

    Stoppers pragma in headers

--- ReadMoleculeFileMGP.h
+++ ReadMoleculeFileMGP.h
@@ -1,6 +1,11 @@
 #ifndef __ReadMolecule_File_MGP_h
 #define __ReadMolecule_File_MGP_h
 
+#ifdef _MSC_VER
+#  pragma once
+#else  // !_MSC_VER
+#endif //  _MSC_VER
+
 #include "ReadMoleculeFileBase.h"
 
 namespace vtk {
