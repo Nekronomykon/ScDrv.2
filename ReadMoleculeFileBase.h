44bd9199d8cd5e433ce3c1afbb5952cf308656b4
Nekronomykon <nekronomykon@gmail.com>
2021-06-23 12:06:30 +0300

    Header stoppers

--- ReadMoleculeFileBase.h
+++ ReadMoleculeFileBase.h
@@ -1,6 +1,11 @@
 #ifndef __Read_MoleculeFile_Base_h
 #define __Read_MoleculeFile_Base_h
 
+#ifdef _MSC_VER
+#  pragma once
+#else  // !_MSC_VER
+#endif //  _MSC_VER
+
 #include <vtkMoleculeReaderBase.h>
 
 namespace vtk {
